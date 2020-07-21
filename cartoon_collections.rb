8b6faa2407bf0989d1538328c74ec8e89b1334b1
Wilson Ng <wilsongch92@gmail.com>
2020-07-21 20:11:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |dwarf, index|
-    p "#{index + 1}. #{dwarf}"
+    p "#{index}. #{dwarf}"
   end
 end
